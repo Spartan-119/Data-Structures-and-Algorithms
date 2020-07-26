ed78330304791feb2ba5754ade6c3aed959d7141
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-07-26 12:45:57 +0530

    Update linked lists.py

--- Linked Lists/linked lists.py
+++ Linked Lists/linked lists.py
@@ -45,7 +45,7 @@ def list_length(self):
 
 # method to insert a new node at the beginning of the LL
 def insert_at_beginning(self, data):
-    new_node = Node() #creating an object of the class Node
+    new_node = Node() #creating an object of the class Node using constructor
     new_node.set_data(data)
     
     if self.length == 0:
@@ -55,3 +55,43 @@ def insert_at_beginning(self, data):
         self.head = new_node
     
     self.length += 1
+    
+#method to insert a new node at the end of the LL
+def insert_at_end(self, data):
+    new_node = Node()
+    new_node.set_data(data)
+    
+    current = self.head()
+    while current.get_next() != None:
+        current = current.get_next()
+    
+    #as soon as it reaches None do the 2 following steps
+    new_node.set_next(None)
+    current = new_node
+    
+    #increment the length of the list by 1
+    self.length += 1
+    
+#method to insert a new node at a position 'pos'
+def insert_at_pos(self, pos, data):
+    if pos > self.length:
+        return None
+    
+    elif pos == 0:
+        self.insert_at_beginning(data)
+    
+    else:
+        new_node = Node()
+        new_node.set_data(data)
+        
+        current = self.head()
+        
+        # traverse to pos-1 before inserting
+        while current.get_next() != pos-1:
+            current.get_next()
+        
+        # now that you have reached pos-1
+        # get ready to insert the new node at pos
+        new_node.set_next(current.get_next())
+        current.set_next(new_node)
+        self.length += 1
