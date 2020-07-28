96f89c8447c3e713b414929327481b2ec2166f72
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-07-28 16:01:05 +0530

    Update Singly Linked Lists Practice.py

--- Linked Lists/Singly Linked Lists Practice.py
+++ Linked Lists/Singly Linked Lists Practice.py
@@ -18,19 +18,44 @@ class LinkedList:
     def printList(self):
         temp = self.head # starting from head
         while(temp):
-            print("Node address: ", temp)
+            #print("Node address: ", temp)
             print("Node data: ", temp.data)
             temp = temp.next
+    
+    # method to insert a node at a position 'pos'
+    def insert_at_pos(self, pos, data):
+        if self.head == None:
+            print("List is empty")
+        
+        # assuming pos < length_of_list
+        else:
+            
+            
+            previous_node = self.head
+            current_node = self.head
+            count = 1
+            
+            
+            while(count != pos):
+                previous_node = current_node
+                current_node = current_node.next
+                count += 1
+            
+            new_node.next = current_node
+            previous_node.next = new_node
+            
         
 if __name__ == '__main__':
     llist = LinkedList()
     llist.head = Node(1)
     second = Node(2)
     third = Node(3)
-    
+    new_node = Node(239)
     # now the linking part
     llist.head.next = second
     second.next = third
+    
+    llist.insert_at_pos(3, new_node)
     
     llist.printList()
     
