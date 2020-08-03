44af68bcceb2b7191c3a46dfa56e7ee2d7ba1932
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-08-04 02:00:41 +0530

    Update Circular Linked Lists.py

--- Linked Lists/Circular Linked Lists.py
+++ Linked Lists/Circular Linked Lists.py
@@ -21,4 +21,31 @@ class CircularLinkedList:
     def __init__(self):
         self.head = None
     
+    def printList(self):
+        if self.head == None:
+            print("The list is empty")
+        else:
+            current_node = self.head
+            count = 1
+            while(current_node.next != self.head):
+                count +=1
+                current_node = current_node.next
+            print("The length of the Circular Linked List is: ", count)
+
+if __name__ == '__main__':
+    cll = CircularLinkedList()
+    cll.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    fourth = Node(4)
+    fifth = Node(5)
     
+    # Now Linking the Nodes
+    cll.head.next = second
+    second.next = third
+    third.next = fourth
+    fourth.next = fifth
+    fifth.next = cll.head
+    
+    # Printing the length of the list
+    cll.printList()
