d3cc6833779772aff9c8f8276d9ea388394a46dc
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-08-28 12:40:21 +0530

    Update Implementing Stacks using Lists.py

--- Stack/Implementing Stacks using Lists.py
+++ Stack/Implementing Stacks using Lists.py
@@ -5,7 +5,7 @@ Implementing Stacks using Lists
 
 Created on Fri Aug 28 12:35:43 2020
 
-@author: abin
+@author: Abin
 """
 
 class Stack:
@@ -23,3 +23,7 @@ class Stack:
                 
     def is_empty(self):
         return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
