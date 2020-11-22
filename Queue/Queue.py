8041686691ddf129787bb21b81ca6f9664dba7de
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-11-23 04:38:26 +0530

    Update Queue.py

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -5,7 +5,7 @@ Created on Mon Nov 23 04:21:03 2020
 @author: Abin
 """
 class Queue:
-    def __init__(self, linit = 5):
+    def __init__(self, limit = 5):
         self.q = []
         self.limit = limit
         self.front = None
@@ -14,4 +14,18 @@ class Queue:
         
     def is_empty(self):
         return self.size <= 0
+    
+    def Enqueue(self, item):
+        if self.size >= self.limit:
+            print("Queue overflow exception!")
+            return
+        else:
+            self.q.append(item)
+        
+        if self.front is None:
+            self.front = self.rear = 0
+        else:
+            self.rear = self.size
+        
+        self.size += 1
     
