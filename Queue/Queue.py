05a79e348a6135cf7836f83f382cfd97bb45f330
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-11-23 04:31:41 +0530

    Starting with Queues

---
+++ Queue/Queue.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 04:21:03 2020
+
+@author: Abin
+"""
+class Queue:
+    def __init__(self, linit = 5):
+        self.q = []
+        self.limit = limit
+        self.front = None
+        self. rear = None
+        self.size = 0
+        
+    def is_empty(self):
+        return self.size <= 0
+    
