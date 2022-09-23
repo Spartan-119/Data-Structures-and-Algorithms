dbd8813f9f497fd95fbc511d5429e0a9f37b3156
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2022-09-23 17:09:48 +0530

    Add files via upload

---
+++ Linked Lists/Detect_loop_in_linked_list.py
@@ -0,0 +1,49 @@
+from Node import Node
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+    
+    def print_list(self):
+        if self.detect_loop():
+            raise Exception('A loop is detected in the linked list. First remove the loop from the list!')
+            return None
+        if self.head == None:
+            return None
+        current_node = self.head
+        while(current_node):
+            print('Node_data:', current_node.data)
+            current_node = current_node.next
+    
+    def push(self, data):
+        new_node = Node(data)                   # creating a new node using the Node object
+        new_node.next = self.head
+        self.head = new_node
+
+    def detect_loop(self) -> bool:
+        '''A linked list is said to have a loop if the next pointer of no node point towards None.'''
+        current_node = self.head
+        hash = set()
+        while(current_node):            
+            if current_node in hash:            # if the current node is already present in the hash
+                return True                     # then return True
+            hash.add(current_node)              # else add the current node to the hash
+            current_node = current_node.next    # update the current node
+
+        return False                            # return False if there is no loop
+            
+
+##############
+# the driver function
+if __name__ == '__main__':
+    ll = LinkedList()
+
+    for i in range(1,6):                       # populating the linked list
+        ll.push(i)
+    
+    ll.head.next.next.next = ll.head           # creating a loop    
+    ll.print_list()
+    if ll.detect_loop():
+        print('A loop is present.')
+    else:
+        print('No loop is present.')
